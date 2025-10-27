92d8b453a12fea443f48d12db566859670519a83
Jason <jasonlow38@gmail.com>
2025-10-27 22:57:46 +0800

    code for best ar based on bic

--- AR(p)/AR(p).R
+++ AR(p)/AR(p).R
@@ -1,1 +1,26 @@
 # AR(p) benchmark model
+
+# load FRED-MD data
+load("data/fredmd.RData")
+
+Y = md
+yy = Y[, "UNRATE"] # get target variable: Unemployment rate
+nprev = 120 #number of out-of-sample observations (test window )
+oosy = tail(yy,nprev) #out-of-sample true values
+trainyy = head(yy, nrow(Y) - nprev) #training sample
+
+source("func-ar.R")
+# select best ar(p) model on bic
+bar1c = runAR(trainyy, lag = 1, type = "bic")
+bar3c = runAR(trainyy, lag = 3, type = "bic")
+bar6c = runAR(trainyy, lag = 6, type = "bic")
+bar12c = runAR(trainyy, lag = 12, type = "bic")
+
+# see selected lags
+bar1c$coef
+bar3c$coef
+bar6c$coef
+bar12c$coef
+
+
+
