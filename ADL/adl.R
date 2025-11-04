94d03deb461d034b2a1a42c4f5742f26e9e9add9
zijing <chakzijing@gmail.com>
2025-11-04 20:41:31 +0800

    Updated ADL script

--- ADL/adl.R
+++ ADL/adl.R
@@ -141,95 +141,24 @@ res_marx6 <- get(load("./ADL/adl_rolling_marx_h6.RData"))
 res_marx12 <- get(load("./ADL/adl_rolling_marx_h12.RData"))
 
 # ============================================
-# Compare forecasts: BIC vs Fixed
+# Compare forecasts: MAF vs MARX vs BIC vs Fixed
 # ============================================
-
-# Get true out-of-sample values
-real <- as.numeric(Y[, 1])
-nprev_eff <- length(res_bic1$pred)
-oosy <- tail(real, nprev_eff)
-
-# h=1
-options(repr.plot.width = 12, repr.plot.height = 6)
-bench1.ts <- ts(cbind(res_bic1$pred, res_fixed1$pred, oosy), 
-                start = c(2010, 1), end = c(2019, 12), freq = 12)
-colnames(bench1.ts) <- c("ARDL-BIC", "ARDL-Fixed", "True")
-plot.ts(bench1.ts[, "True"], main = "1-step Ahead Forecast", 
-        cex.axis = 1.2, lwd = 2, col = "black", 
-        ylab = "Change in Unemployment Rate", ylim = range(bench1.ts))
-lines(bench1.ts[, "ARDL-BIC"], col = "blue", lwd = 1.5)
-lines(bench1.ts[, "ARDL-Fixed"], col = "red", lwd = 1.5, lty = 2)
-legend("topright", 
-       legend = c("ARDL-BIC", "ARDL-Fixed", "Actual"),
-       col = c("blue", "red", "black"),
-       lty = c(1, 2, 1), lwd = c(1.5, 1.5, 2), 
-       bty = "n", cex = 0.8)
-
-# h=3
-bench3.ts <- ts(cbind(res_bic3$pred, res_fixed3$pred, oosy), 
-                start = c(2010, 1), end = c(2019, 12), freq = 12)
-colnames(bench3.ts) <- c("ARDL-BIC", "ARDL-Fixed", "True")
-plot.ts(bench3.ts[, "True"], main = "3-step Ahead Forecast", 
-        cex.axis = 1.2, lwd = 2, col = "black", 
-        ylab = "Change in Unemployment Rate", ylim = range(bench3.ts))
-lines(bench3.ts[, "ARDL-BIC"], col = "blue", lwd = 1.5)
-lines(bench3.ts[, "ARDL-Fixed"], col = "red", lwd = 1.5, lty = 2)
-legend("topright", 
-       legend = c("ARDL-BIC", "ARDL-Fixed", "Actual"),
-       col = c("blue", "red", "black"),
-       lty = c(1, 2, 1), lwd = c(1.5, 1.5, 2), 
-       bty = "n", cex = 0.8)
-
-# h=6
-bench6.ts <- ts(cbind(res_bic6$pred, res_fixed6$pred, oosy), 
-                start = c(2010, 1), end = c(2019, 12), freq = 12)
-colnames(bench6.ts) <- c("ARDL-BIC", "ARDL-Fixed", "True")
-plot.ts(bench6.ts[, "True"], main = "6-step Ahead Forecast", 
-        cex.axis = 1.2, lwd = 2, col = "black", 
-        ylab = "Change in Unemployment Rate", ylim = range(bench6.ts))
-lines(bench6.ts[, "ARDL-BIC"], col = "blue", lwd = 1.5)
-lines(bench6.ts[, "ARDL-Fixed"], col = "red", lwd = 1.5, lty = 2)
-legend("topright", 
-       legend = c("ARDL-BIC", "ARDL-Fixed", "Actual"),
-       col = c("blue", "red", "black"),
-       lty = c(1, 2, 1), lwd = c(1.5, 1.5, 2), 
-       bty = "n", cex = 0.8)
-
-# h=12
-bench12.ts <- ts(cbind(res_bic12$pred, res_fixed12$pred, oosy), 
-                 start = c(2010, 1), end = c(2019, 12), freq = 12)
-colnames(bench12.ts) <- c("ARDL-BIC", "ARDL-Fixed", "True")
-plot.ts(bench12.ts[, "True"], main = "12-step Ahead Forecast", 
-        cex.axis = 1.2, lwd = 2, col = "black", 
-        ylab = "Change in Unemployment Rate", ylim = range(bench12.ts))
-lines(bench12.ts[, "ARDL-BIC"], col = "blue", lwd = 1.5)
-lines(bench12.ts[, "ARDL-Fixed"], col = "red", lwd = 1.5, lty = 2)
-legend("topright", 
-       legend = c("ARDL-BIC", "ARDL-Fixed", "Actual"),
-       col = c("blue", "red", "black"),
-       lty = c(1, 2, 1), lwd = c(1.5, 1.5, 2), 
-       bty = "n", cex = 0.8)
-par(mfrow = c(1, 1))
-
-# ============================================
-# Compare forecasts: MAF vs MARX vs BIC
-# ============================================
-# Get true out-of-sample values
-
 # Helper: plot ARDL benchmark for a given horizon
-plot_ardl_bench <- function(res_bic, res_fixed, real,
+plot_ardl_bench <- function(res_bic, res_fixed, res_maf, res_marx, real,
                             h, end = c(2019, 12), freq = 12,
                             ylab = "Change in Unemployment Rate",
                             main = NULL) {
-  # Align lengths (just in case)
-  L <- min(length(res_bic$pred), length(res_fixed$pred))
-  stopifnot(L > 0)
-  bic   <- as.numeric(res_bic$pred)[seq_len(L)]
-  fixed <- as.numeric(res_fixed$pred)[seq_len(L)]
-  true  <- tail(as.numeric(real), L)
+  # Align lengths in case of any discrepancies
+     L <- min(length(res_bic$pred), length(res_fixed$pred))
+     stopifnot(L > 0)
+     bic   <- as.numeric(res_bic$pred)[seq_len(L)]
+     fixed <- as.numeric(res_fixed$pred)[seq_len(L)]
+     maf   <- as.numeric(res_maf$pred)[seq_len(L)]     
+     marx  <- as.numeric(res_marx$pred)[seq_len(L)]
+     true  <- tail(as.numeric(real), L)
 
-  M <- cbind(bic, fixed, true)
-  colnames(M) <- c("ARDL-BIC", "ARDL-Fixed", "True")
+  M <- cbind(bic, fixed, maf, marx, true)
+  colnames(M) <- c("ARDL-BIC", "ARDL-Fixed", "ARDL-MAF", "ARDL-MARX", "True")
 
   # Use fixed end date; ts() computes the correct start automatically
   obj <- ts(M, end = end, frequency = freq)
@@ -241,10 +170,12 @@ plot_ardl_bench <- function(res_bic, res_fixed, real,
           ylab = ylab, ylim = range(obj, na.rm = TRUE))
   lines(obj[, "ARDL-BIC"],   col = "blue", lwd = 1.5)
   lines(obj[, "ARDL-Fixed"], col = "red",  lwd = 1.5, lty = 2)
+  lines(obj[, "ARDL-MAF"],   col = "green", lwd = 1.5)
+  lines(obj[, "ARDL-MARX"],  col = "purple", lwd = 1.5)
   legend("topright",
-         legend = c("ARDL-BIC", "ARDL-Fixed", "Actual"),
-         col = c("blue", "red", "black"),
-         lty = c(1, 2, 1), lwd = c(1.5, 1.5, 2),
+         legend = c("ARDL-BIC", "ARDL-Fixed", "ARDL-MAF", "ARDL-MARX", "Actual"),
+         col = c("blue", "red", "green", "purple", "black"),
+         lty = c(1, 2, 1, 1, 1), lwd = c(1.5, 1.5, 1.5, 1.5, 2),
          bty = "n", cex = 0.8)
 }
 
@@ -256,16 +187,16 @@ end_date <- c(2019, 12)
 options(repr.plot.width = 12, repr.plot.height = 6)
 
 # h = 1
-plot_ardl_bench(res_bic1,  res_fixed1,  real, h = 1,  end = end_date)
+plot_ardl_bench(res_bic1,  res_fixed1,  res_maf1,  res_marx1,  real, h = 1,  end = end_date)
 
 # h = 3
-plot_ardl_bench(res_bic3,  res_fixed3,  real, h = 3,  end = end_date)
+plot_ardl_bench(res_bic3,  res_fixed3,  res_maf3,  res_marx3,  real, h = 3,  end = end_date)
 
 # h = 6
-plot_ardl_bench(res_bic6,  res_fixed6,  real, h = 6,  end = end_date)
+plot_ardl_bench(res_bic6,  res_fixed6,  res_maf6,  res_marx6,  real, h = 6,  end = end_date)
 
 # h = 12
-plot_ardl_bench(res_bic12, res_fixed12, real, h = 12, end = end_date)
+plot_ardl_bench(res_bic12, res_fixed12, res_maf12, res_marx12, real, h = 12, end = end_date)
 
 par(mfrow = c(1, 1))
 
